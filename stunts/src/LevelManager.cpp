ac78eeb37807750ab9fcec8f2d02cf9436d4630e
psyborg <psyborg>
2005-07-02 06:33:23 +0000

    Missing

---
+++ stunts/src/LevelManager.cpp
@@ -0,0 +1,292 @@
+/* Stunts 2005 - A remake of the game Stunts
+ *
+ * Copyright (C) 2005
+ *                    Stunts 2005 Workgroup,
+ *                    http://developer.berlios.de/projects/stunts2005
+ *
+ * Maintainer:        Christian Morbach <GameDevelopment@paratronic.de>
+ *                    Art Tevs <tevs@mpi-sb.mpg.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
+
+
+//------------------------------------------------------------------------------
+//--- includes
+//------------------------------------------------------------------------------
+#include "LevelManager.hpp"
+#include "Utils.hpp"
+
+#include <boost/shared_ptr.hpp>
+
+using boost::shared_ptr;
+using namespace std;
+using namespace Ogre;
+
+//------------------------------------------------------------------------------
+//--- namespace stunts
+//------------------------------------------------------------------------------
+namespace stunts
+{
+
+	//--------------------------------------------------------------------------
+	CLevelManager::CLevelManager():mLevel(0)
+	{
+	
+		mShouldLoadLevel = false;
+		mIsCurrentLoaded = false;
+		
+		registerAllVars();
+	}
+	
+	//--------------------------------------------------------------------------
+	CLevelManager::~CLevelManager()
+	{
+		unload();
+		
+		mLevel.reset();		
+		
+		deregisterAllVars();
+	}
+
+
+	//--------------------------------------------------------------------------
+	int32 CLevelManager::loadLevel(std::string name)
+	{
+		// check now, if we does support such a level
+		if (!hasGotLevel(name)) return NO_SUCH_LEVEL_FOUND;
+
+		// first we check whenever the level is already loaded
+		if (mIsCurrentLoaded && mCurrentLevel == name) return LEVEL_ALREADY_LOADED;
+		
+		// if another level is loaded, so error code
+		if (mIsCurrentLoaded && mCurrentLevel != name) return ANOTHER_LEVEL_IS_LOADED;
+		
+		// create new level object
+		mLevel.reset(new CLevel());
+		
+		// get the filename of the level file
+		std::string fileName = mLevelPath + getLevelData(name)->path + "/level.xml";
+		
+		nrLog.Log(NR_LOG_APP, "CLevelManager: Load level from \"%s\"", fileName.c_str());
+		
+		// load the level
+		if (mLevel->loadLevel(fileName)) return LEVEL_LOAD_ERROR;
+		mLevel->start();
+				
+		mIsCurrentLoaded = true;
+		mCurrentLevel = name;
+		
+		// OK
+		return OK;
+	}
+	
+	
+	//--------------------------------------------------------------------------
+	int32 CLevelManager::unloadLevel(std::string name)
+	{
+		// check now, if we does support such a level
+		if (!hasGotLevel(name)) return NO_SUCH_LEVEL_FOUND;
+
+		// first we check whenever the level is already loaded
+		if (!mIsCurrentLoaded && mCurrentLevel == name) return LEVEL_NOT_LOADED;
+		
+		// if another level is loaded, so error code
+		if (mIsCurrentLoaded && mCurrentLevel != name) return ANOTHER_LEVEL_IS_LOADED;
+
+		nrLog.Log(NR_LOG_APP, "CLevelManager: Unload level \"%s\"", name.c_str());
+
+		// create new level object
+		mLevel->stop();
+		mLevel.reset(0);
+				
+		mIsCurrentLoaded = false;
+		mCurrentLevel = name;
+		
+		// OK
+		return OK;
+	}
+
+	//--------------------------------------------------------------------------
+	int32 CLevelManager::reloadLevel(std::string name)
+	{
+		// First unload the level
+		int32 ret = unloadLevel(name);
+		if (ret != OK) return ret;
+		
+		// now load the level
+		return loadLevel(name);
+	}
+	
+	//--------------------------------------------------------------------------
+	CLevelManager::LevelData* CLevelManager::getLevelData(const std::string& name)
+	{
+		std::vector<LevelData>::iterator it = mLevelList.begin();
+		
+		while (it != mLevelList.end())
+		{
+			// Level was found
+			if (it->name == name) return &(*it);
+		}
+		
+		// no such level name was found
+		return NULL;
+	}
+
+	//--------------------------------------------------------------------------
+	bool CLevelManager::hasGotLevel(const std::string& name)
+	{
+		return getLevelData(name) != NULL;	
+	}
+		
+	
+	//--------------------------------------------------------------------------
+	void CLevelManager::registerAllVars()
+	{
+		nrSettingsRegisterString(mLevelToLoad, 	"level_file");
+
+		nrSettingsRegister(bool, mShouldLoadLevel, 	"load_level");
+	}
+	
+
+	//--------------------------------------------------------------------------
+	void CLevelManager::deregisterAllVars()
+	{
+		nrSettings.deregisterVariable("level_file");
+		nrSettings.deregisterVariable("load_level");
+	}
+	
+	
+	//--------------------------------------------------------------------------
+	int32 CLevelManager::loadLevelDescriptions(const std::string& fileName)
+	{
+		nrLog.Log(NR_LOG_APP, "CLevelManager: Load level definitions from \"%s\"", fileName.c_str());
+		
+		// we open the file for parsing.
+		// Later we can open the file through the virtual file system if we had got more time
+		// for development
+		shared_ptr<TiXmlDocument> mLevelDoc (new TiXmlDocument(fileName.c_str()));
+		if (!mLevelDoc->LoadFile())
+		{
+			nrLog.Log(NR_LOG_APP, "CLevelManager: There is an error occurs by loading of level description \"%s\"", fileName.c_str());
+			return CONTENT_FILE_NOT_FOUND;
+		}
+
+		// Load elements form the level file and handle with them in according way
+		TiXmlElement* rootElem = mLevelDoc->FirstChildElement("levels");
+
+		// check if the root element is correct one
+		if (!rootElem)
+		{
+			nrLog.Log(NR_LOG_APP, "CLevelManager: Content file does not contain valid data");
+			return CONTENT_FILE_CORRUPTED;
+		}
+
+		// get the path from the file name
+		mLevelPath = getPathFromFileName(fileName);
+
+		// scan through all levels we can found here
+		for (TiXmlElement* elem = rootElem->FirstChildElement("level"); elem != 0;
+					elem = rootElem->NextSiblingElement("level"))
+        {
+
+			TiXmlElement* sElem = NULL;
+
+			// Level data
+			LevelData data;
+			
+			// get the name
+			sElem = elem->FirstChildElement("name");
+			if (sElem)
+			{
+				data.name = sElem->Attribute("name");
+				data.path = sElem->Attribute("path");
+			}
+			
+			// get creator data
+			sElem = elem->FirstChildElement("author");
+			if (sElem)
+			{
+				data.author = sElem->Attribute("name");
+				data.datestr = sElem->Attribute("datestr");
+			}
+			
+			// store the data in a supported list of levels
+			mLevelList.push_back(data);
+			
+			// Some log info
+			nrLog.Log(NR_LOG_APP, "CLevelManager: Found level \"%s\" (path \"%s%s\") created by %s", 
+					data.name.c_str(), mLevelPath.c_str(), data.path.c_str(), data.author.c_str());
+		}
+		
+		// OK
+		return OK;
+	}
+	
+	
+	//--------------------------------------------------------------------------
+	nrResult CLevelManager::taskInit()
+	{
+		// OK
+		return NR_OK;
+	
+	}
+	
+	//--------------------------------------------------------------------------
+	nrResult CLevelManager::taskStart()
+	{
+		// OK
+		return NR_OK;
+	
+	}
+
+	//--------------------------------------------------------------------------
+	nrResult CLevelManager::taskUpdate()
+	{
+		// if we are forced to load the level file
+		if (mShouldLoadLevel && (mLevelToLoad.length() > 0) && (COgreTask::GetSingleton().mSceneMgr != NULL)){
+			mShouldLoadLevel = false;
+
+			//load the level and its physics
+			loadLevel(mLevelToLoad);
+		}
+		
+		// update the level
+		if (mLevel)
+			mLevel->update();
+		
+		// OK
+		return NR_OK;
+	
+	}
+
+	//--------------------------------------------------------------------------
+	nrResult CLevelManager::taskStop()
+	{
+		// Unload the level
+		unload();
+		
+		// OK
+		return NR_OK;
+	}
+	
+			
+};
+// namespace stunts
+
+
+
+
---
+++ stunts/src/LevelManager.hpp
@@ -0,0 +1,193 @@
+/* Stunts 2005 - A remake of the game Stunts
+ *
+ * Copyright (C) 2005
+ *                    Stunts 2005 Workgroup,
+ *                    http://developer.berlios.de/projects/stunts2005
+ *
+ * Maintainer:        Art Tevs <tevs@mpi-sb.mpg.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
+
+
+#ifndef _LEVEL_MANAGER_HPP_
+#define _LEVEL_MANAGER_HPP_
+
+
+//------------------------------------------------------------------------------
+//--- predeclare this class
+//------------------------------------------------------------------------------
+
+namespace stunts
+{
+	class CLevelManager;
+}
+
+//------------------------------------------------------------------------------
+//--- includes
+//------------------------------------------------------------------------------
+#include <OGRE/Ogre.h>
+
+#include <nrEngine/nrEngine.h>
+
+#include "External/tinyxml/tinyxml.h"
+
+#include "Level.hpp"
+
+#include <boost/scoped_ptr.hpp>
+
+//------------------------------------------------------------------------------
+//--- namespace stunts
+//------------------------------------------------------------------------------
+namespace stunts
+{
+	/**
+	 * Manager that manages level loading and unloading. Also it can read
+	 * files containing information about all available levels, so you can
+	 * load levels by just giving their names. 
+	 *
+	 * @note At now there is only support for one level loaded at the same time.
+	 * 		In the future the manager can be rewritten to support more than one
+	 *		level simultaneously.
+	 **/
+	class CLevelManager : public nrISingletonTask<CLevelManager>, public nrIChangeable
+	{
+		private:
+			//! this structure holds information about a level
+			struct LevelData
+			{
+				std::string		name;
+				std::string 	path;
+				std::string		author;
+				std::string		datestr;
+				
+			};
+			
+			//! Here we store the level object
+			boost::scoped_ptr<CLevel>	mLevel;
+			
+			//! Vector containing all levels we are using in the game
+			std::vector<LevelData>		mLevelList;
+			
+			//! Name of the level, that is currently loaded
+			std::string					mCurrentLevel;
+			
+			//! If true, so we have a level that is currently loaded
+			bool						mIsCurrentLoaded;
+			
+			//! String containg information about the path containg levels
+			std::string					mLevelPath;
+			
+			//! Store here the name of the level to be loaded at the next cycle (setted by the settings manager)
+			std::string					mLevelToLoad;
+			
+			//! If 1 so the level will be loaded at the next cycle
+			bool						mShouldLoadLevel;
+			
+			//----------------------------------------------------
+			// Methods
+			//----------------------------------------------------
+			LevelData*		getLevelData(const std::string& name);
+			
+		public:
+			
+			// Error codes
+			enum{
+				OK = 0,
+				NO_SUCH_LEVEL_FOUND,
+				LEVEL_ALREADY_LOADED,
+				LEVEL_NOT_LOADED,
+				ANOTHER_LEVEL_IS_LOADED,
+				LEVEL_LOAD_ERROR,
+				CONTENT_FILE_NOT_FOUND,
+				CONTENT_FILE_CORRUPTED
+			};
+			
+			// Constructor & Desturctor
+			CLevelManager ();
+			~CLevelManager();
+			
+			
+			/** Unload all currently opened levels
+			*/
+			int32 unload() 
+			{ 
+				if (mIsCurrentLoaded) 
+					return unloadLevel(mCurrentLevel);
+					
+				return OK;
+			}
+			
+			/** Load the level with specfied name
+			*/
+			int32 loadLevel(std::string name);
+			
+			/** Unload the level with specfied name
+			*/
+			int32 unloadLevel(std::string name);
+						
+			/** Reload the level with specfied name
+			*/
+			int32 reloadLevel(std::string name);
+			
+			
+			/** Load the file containg level list, which show us all levels 
+			 *  we can use in the game
+			 *  @param fileName FileName of the XML-File containing level descriptions
+			 *  @return 0 if load was successful 
+			 */
+			int32 loadLevelDescriptions(const std::string& fileName);
+			
+			
+			/** Get the list containing all levels we can load
+			*/
+			//std::vector<std::string> getLevelList();
+			
+			/** Returns true if such a level names exists in our database.
+			*/
+			bool hasGotLevel(const std::string& name);
+			
+			
+			//-----------------------------------------
+			// Task Interface
+			//-----------------------------------------
+			nrResult taskInit();
+			nrResult taskStart();
+			nrResult taskStop();
+			nrResult taskUpdate();
+			
+			const char* taskGetName() { return "CLevelManager"; }
+			
+			//----------------------------------------
+			// Changeable Interface
+			//----------------------------------------
+
+			//! Register all used variables by the settings manager
+			void	registerAllVars();
+
+			//! Remove all used variables from the settings manager
+			void	deregisterAllVars();
+
+			
+	};
+
+};
+// namespace stunts
+
+#endif //_LEVEL_HPP_
+
+
+
